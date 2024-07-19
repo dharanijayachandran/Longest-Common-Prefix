4114805797c14f87c317eaa293cb52142abc40ba
dharanijayachandran <dharanipriya18it@gmail.com>
2024-07-19 15:41:54 +0530

    Longest Common Prefix

---
+++ Longest Common Prefix.js
@@ -0,0 +1,18 @@
+var longestCommonPrefix = function(strs) {
+    let sorted = strs.sort();
+    console.log(sorted)
+    let first = sorted[0];
+    let last = sorted[sorted.length - 1]
+   let prefix = [];
+    for(let i=0;i<first.length;i++){
+        if(first[i] == last[i]){
+            prefix.push(first[i]);
+        }else{
+            break;
+        }
+    }
+    return prefix;
+}
+
+let strs = ["flight","flower","flow"];
+console.log(longestCommonPrefix(strs).join(''))
